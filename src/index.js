5feea8529ed62e20f8cc7e4a803d69ada5aa68f6
Uliana Kozak <una.kozak@mail.ru>
2021-03-15 20:58:39 +0300

    feat: solve reverse-int problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    return n < 0
+        ? n.toString().slice(1).split("").reverse().join("")
+        : n.toString().split("").reverse().join("");
+};
